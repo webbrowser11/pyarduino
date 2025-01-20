ba41c81ba0b4e937bf6c4c1d91d3afb75d4715e2
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-20 14:47:28 -0800

    project

---
+++ pyarduino.ino
@@ -0,0 +1,14 @@
+/* Py Arduino Tool 2025, webbrowser11 */
+/* Run Python Code From Your Arduino. */
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("run_python_code");
+}
+
+void loop() {
+    if (Serial.avalible() > 0) {
+        String response = Serial.readString();
+        Serial.pritln(response)
+    }
+}
---
+++ settings.py
@@ -0,0 +1,21 @@
+# Py Arduino Tool 2025, webbrowser11
+# Run Python Code From Your Arduino.
+
+import serial # IGNORE
+import time # IGNORE
+
+arduino = serial.Serial(port='', baudrate=9600, timeout=1) # In the port section enter your arduino's port.
+
+time.sleep(3) # IGNORE
+
+def run_python_code(): # IGNORE
+    # YOUR PYTHON CODE GOES HERE!
+    return "Executed." # IGNORE
+
+command = arduino.readline().decode("utf-8").strip() # IGNORE
+if command == "run_python_code": # IGNORE
+    print("Looks like the arduino is to run your code!") # IGNORE
+    result = run_python_code() # IGNORE
+    arduino.write((result + '\n').encode('utf-8')) # IGNORE
+
+arduino.close() # IGNORE
